71ab24da13a58f1c839ef58b14d118b2b030d70e
MatchaTeaLeaf <51747904+matchatealeaf@users.noreply.github.com>
2020-01-25 14:20:24 +0000

    Fixed github stupid indentation

--- cogs/fetchprice.py
+++ cogs/fetchprice.py
@@ -239,11 +239,11 @@ class FetchPrice(commands.Cog):
         itemIDs = []
         jDists = []
 
-		try:
-        	with open('item_data.json', 'r', encoding='utf-8') as inFile:
-            	data = json.load(inFile)
-		except Exception as e:
-			print(e)
+	try:
+		with open('item_data.json', 'r', encoding='utf-8') as inFile:
+			data = json.load(inFile)
+	except Exception as e:
+		print(e)
 
         # Loop through each item in item_data.json
         # Store distance and item index of each item
